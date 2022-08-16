a54b8b764820012d3a9d156bd1159dcaf3c20707
hisphone <87437814+hisphone@users.noreply.github.com>
2022-08-16 15:37:38 +0800

    ToString
    rust auto impl ToString for T:Display

--- src/document/spreadsheet/cell.rs
+++ src/document/spreadsheet/cell.rs
@@ -25,23 +25,18 @@ pub enum CellValue {
     Raw(String),
 }
 
-impl CellValue {
-    pub fn to_string(&self) -> String {
-        match self {
-            CellValue::Null => "".to_string(),
-            CellValue::String(v) => v.clone(),
-            CellValue::Raw(v) => v.clone(),
-            CellValue::Bool(_b) => panic!("unsupported cell type: bool"),
-            CellValue::Double(f) => format!("{}", f),
-            CellValue::DateTime(datetime, format) => format!("{}", datetime.format(&format)),
-            _ => unimplemented!(),
-        }
-    }
-}
-
 impl Display for CellValue {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.to_string())
+       
+        match self {
+            CellValue::Null =>  write!(f, "{}", self.to_string()),
+            CellValue::String(v) =>  write!(f, "{}", v),
+            CellValue::Raw(v) => write!(f, "{}", v),
+            CellValue::Bool(_v) => panic!("unsupported cell type: bool"),
+            CellValue::Double(v) => write!(f, "{}", v),
+            CellValue::DateTime(datetime, format) => write!(f, "{}", datetime.format(&format)),
+            _ => unimplemented!(),
+        }
     }
 }
 
